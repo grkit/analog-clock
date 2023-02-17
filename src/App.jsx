9f3cb97b1d3a063d6c8925aa50ab706b4610aebe
grkit <grkit@Gurkirats-MacBook-Air.local>
2023-02-17 10:57:03 -0800

    clock

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Clock from './assets/Components/Clock'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Clock/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/assets/Components/Clock.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import '../../App.css'
+
+const Clock = () => {
+   
+   let time = new Date().toLocaleTimeString();
+   const [currentTime, setCurrentTime] = useState(time);
+
+   const updateTime =() =>{
+      let time = new Date().toLocaleTimeString();
+      setCurrentTime(time);
+   }
+
+   setInterval(updateTime, 1000);
+
+  return (
+    <div className='clock'>
+      <h1>{currentTime}</h1>
+    </div>
+  )
+}
+
+export default Clock
